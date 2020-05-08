3008d173993c7f70f54ec42fc0bcaf508a96eaf0
harpuiasaber <harpuiasaber@harpuiasaber-Inspiron-5558>
2020-05-08 11:52:52 +0700

    first

---
+++ BHXH/src/main/java/webservice/BHXH/BhxhApplication.java
@@ -0,0 +1,13 @@
+package webservice.BHXH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhxhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhxhApplication.class, args);
+	}
+
+}
